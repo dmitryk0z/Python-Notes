d486f82c6d0f3f01653f443375db91e4f1375f3a
dmitryk0z <79474744+dmitryk0z@users.noreply.github.com>
2022-03-12 17:18:24 +0000

    Update bubble_sort.py

--- Sorting Algorithms/bubble_sort.py
+++ Sorting Algorithms/bubble_sort.py
@@ -1,3 +1,37 @@
+"""
+Bubble Sort
+
+1) What it does?
+    Bubble Sort is based on the idea of repeatedly comparing pairs of adjacent elements and then swapping their
+    positions if they exist in the wrong order. If a list of elements has to be sorted in ascending order, then Bubble
+    Sort will start by comparing the first element of the list with the second one. If the first element is greater
+    than the second one, it will swap both elements and move on to compare the second and third element, and so on.
+
+2) When to use?
+    Bubble Sort is often considered an inefficient sorting tool since it must exchange items before the final
+    locations of the elements are known. However, Bubble Sort is a good strategy to organize elements that are already
+    partially sorted.
+
+3) Stability:
+    Stable (this is the ability of a sorting algorithm to keep the relative order of equal keys in a file).
+
+4) Time Complexity:
+
+    -   Worst Case and Average Case (Quadratic Time Complexity - O(n^2)): Worst case occurs when array is reverse
+        sorted.
+    -   Best Case (Linear Time Complexity - O(n)): Best case occurs when array is already sorted.
+
+5) Algorithm Design Strategy:
+    Brute Force.
+
+6) Sorting In Place?
+    Yes (means no additional storage space is needed to perform sorting).
+
+7) Adaptive (if it takes advantage of already 'sorted' elements in the list that is to be sorted)?
+    Yes.
+"""
+
+
 def bubble_sort(arr):
     n = len(arr)
 
